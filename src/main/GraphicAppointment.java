1817f38e51577590f37dccc8565655121b7f5742
Thomas Gautvedt <thomasgautv@hotmail.com>
2014-03-17 13:43:58 +0100

    Removed code that was no longer in use

--- src/main/GraphicAppointment.java
+++ src/main/GraphicAppointment.java
@@ -82,15 +82,7 @@ public class GraphicAppointment extends JPanel implements MouseMotionListener {
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		/*final int x = e.getX();
-        final int y = e.getY();
-        // only display a hand if the cursor is over the items
-        final Rectangle cellBounds = list.getCellBounds(0, list.getModel().getSize() - 1);
-        if (cellBounds != null && cellBounds.contains(x, y)) {
-            
-        } else {
-            setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-        }*/
+		// Put stuff here
 	}
 
 }
