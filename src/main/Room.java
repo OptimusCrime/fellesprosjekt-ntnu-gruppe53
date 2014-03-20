2ce9a5c37f8b0830327868c884adce8c7d81da5c
Thomas Gautvedt <thomasgautv@hotmail.com>
2014-03-20 15:18:01 +0100

    Made changes to reflect the current structure

--- src/main/Room.java
+++ src/main/Room.java
@@ -21,8 +21,6 @@ public class Room implements CalendarObjects {
 	private int id;
 	private String name;
 	private int capacity;
-	
-	private ArrayList<ArrayList<Date>> availability;
 	/*
 	 * Constructor
 	 */
@@ -74,36 +72,5 @@ public class Room implements CalendarObjects {
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 		this.gui.reflectChange("room", "capacity", this);
-	}
-	
-	public boolean isAvailable(Date startDate, Date endDate) {
-		if (startDate.after(endDate) || startDate.equals(endDate)) {
-			System.out.println("Starting date cant be after or equal to ending date");
-		}
-		else {
-			for (int i = 0; i < availability.size(); i++) {
-				if (startDate.equals(getEndDate(i)) || getStartDate(i).equals(endDate)) {
-					return false;
-				} 
-				if (startDate.before(getEndDate(i)) && endDate.after(getStartDate(i))) {
-					return false;
-				}
-				if (getStartDate(i).before(endDate) && getEndDate(i).after(startDate)) {
-					return false;
-				}
-			}
- 		}
- 		return true;
-	}
-	public void reserve(Date startDate, Date endDate) {
-		//availability.add(ArrayList<Date>);
-		//availability.get(availability.size() - 1).add(startDate);
-		//availability.get(availability.size() - 1).add(endDate);
-	}
-	private Date getStartDate(int index) {
-		return availability.get(index).get(0);
-	}
-	private Date getEndDate(int index) {
-		return availability.get(index).get(1);
 	}
 }
